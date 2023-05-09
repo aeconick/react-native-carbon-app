f928e61bd920f5c810ef7ec8f939cbab009dd4ae
Nikolay Cherepov <aeconick@gmail.com>
2023-05-09 19:15:54 +0300

    Add icons constant

---
+++ CarbonApp/src/constants/icons.js
@@ -0,0 +1,12 @@
+const icons = {
+    Meal: "utensils",
+    Transport: "plane",
+    Streaming: "video",
+    Purchase: "credit-card",
+    Fashion: "tshirt",
+    Food: "apple-alt",
+    Electricity: "plug",
+    Custom: "tools",
+};
+
+export default icons;
