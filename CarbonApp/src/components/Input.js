6a911ad23def7b48acd22c86b4275b7c0048061e
Nikolay Cherepov <aeconick@gmail.com>
2023-05-10 20:00:05 +0300

    Add input component

---
+++ CarbonApp/src/components/Input.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import {View, Text, TextInput, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+const Input = ({
+  label,
+  iconName,
+  error,
+  password,
+  onFocus = () => {},
+  ...props
+}) => {
+  const [hidePassword, setHidePassword] = React.useState(password);
+  const [isFocused, setIsFocused] = React.useState(false);
+  return (
+    <View style={{marginBottom: 20}}>
+      <Text style={style.label}>{label}</Text>
+      <View
+        style={[
+          style.inputContainer,
+          {
+            borderColor: error
+              ? "red"
+              : isFocused
+              ? "seagreen"
+              : "honeydew",
+            alignItems: 'center',
+          },
+        ]}>
+        <Icon
+          name={iconName}
+          style={{color: "seagreen", fontSize: 22, marginRight: 10}}
+        />
+        <TextInput
+          autoCorrect={false}
+          onFocus={() => {
+            onFocus();
+            setIsFocused(true);
+          }}
+          onBlur={() => setIsFocused(false)}
+          secureTextEntry={hidePassword}
+          style={{color: "seagreen", flex: 1}}
+          {...props}
+        />
+        {password && (
+          <Icon
+            onPress={() => setHidePassword(!hidePassword)}
+            name={hidePassword ? 'eye-outline' : 'eye-off-outline'}
+            style={{color: "seagreen", fontSize: 22}}
+          />
+        )}
+      </View>
+      {error && (
+        <Text style={{marginTop: 7, color: "red", fontSize: 12}}>
+          {error}
+        </Text>
+      )}
+    </View>
+  );
+};
+
+const style = StyleSheet.create({
+  label: {
+    marginVertical: 5,
+    fontSize: 14,
+    color: "grey",
+  },
+  inputContainer: {
+    height: 55,
+    backgroundColor: "honeydew",
+    flexDirection: 'row',
+    paddingHorizontal: 15,
+    borderWidth: 0.5,
+  },
+});
+
+export default Input;
