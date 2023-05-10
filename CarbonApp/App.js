3bcf7bc004d2625ba22bad411fd06c06f4951994
Nikolay Cherepov <aeconick@gmail.com>
2023-05-10 16:31:55 +0300

    Update navigation

--- CarbonApp/App.js
+++ CarbonApp/App.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import Tabs from "./src/components/Tabs";
+import Tabs from "./src/navigation/Tabs";
 
-const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Tabs />
+      <Stack.Navigator >
+        <Stack.Screen
+          options={{
+            headerShown: false,
+          }}
+          name="Main"
+          component={Tabs}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
