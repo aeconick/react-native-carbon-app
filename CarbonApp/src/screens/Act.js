53bba0c4982eef11fde2a257a63cefdfb3937672
Nikolay Cherepov <aeconick@gmail.com>
2023-05-11 11:31:44 +0300

    Add plain Act screen

---
+++ CarbonApp/src/screens/Act.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const Act = () => {
+    return (
+        <View>
+            <Text>Act</Text>
+        </View>
+    );
+};
+
+export default Act;
+
+const styles = StyleSheet.create({});
