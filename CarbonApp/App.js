5b6fec5fa331a228efdbe046bf220a7e8824729e
Nikolay Cherepov <aeconick@gmail.com>
2023-05-11 12:01:44 +0300

    Update App

--- CarbonApp/App.js
+++ CarbonApp/App.js
@@ -1,23 +1,33 @@
+import 'react-native-gesture-handler';
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-import Tabs from "./src/navigation/Tabs";
+import AuthNavigator from './src/navigation/AuthNavigator';
+import TabNavigator from './src/navigation/TabNavigator';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  let isAuthenticated = false;
+  const readData = async () => {
+    try {
+      const value = await AsyncStorage.getItem(STORAGE_KEY);
+
+      if (value !== null) {
+        isAuthenticated = true;
+      }
+    } catch (e) {
+      alert('Failed to fetch the input from storage');
+    }
+  };
+  console.log(isAuthenticated);
   return (
     <NavigationContainer>
-      <Stack.Navigator >
-        <Stack.Screen
-          options={{
-            headerShown: false,
-          }}
-          name="Main"
-          component={Tabs}
-        />
-      </Stack.Navigator>
+      {isAuthenticated
+        ? <TabNavigator />
+        : <AuthNavigator />}
     </NavigationContainer>
   );
 };
