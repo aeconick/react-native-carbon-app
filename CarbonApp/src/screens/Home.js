50c7501f639231b4d2b06fed4c0141855b0ce2d3
Nikolay Cherepov <aeconick@gmail.com>
2023-05-11 15:26:41 +0300

    Update Home component with logs

--- CarbonApp/src/screens/Home.js
+++ CarbonApp/src/screens/Home.js
@@ -1,11 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 
+import AsyncStorage from '@react-native-async-storage/async-storage'; //TODO: DELETE
+
 const Home = () => {
+  let userData = null;
+
+  const readData = async () => {
+    try {
+      const value = await AsyncStorage.getItem("userData");
+
+      if (value) {
+        userData = JSON.parse(value);
+      }
+    } catch (e) {
+      alert('Failed to fetch the input from storage');
+    }
+  };
+
+  readData()
+    .then(() => console.log("welcome home ->", userData))
+    .catch((e) => console.log(e));
   return (
-    <View>
+    <SafeAreaView>
       <Text>Home</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
