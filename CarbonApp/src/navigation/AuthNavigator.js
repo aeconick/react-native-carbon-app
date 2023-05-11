65896917e7605885ce25350df0bc18bf96c5e03e
Nikolay Cherepov <aeconick@gmail.com>
2023-05-11 12:00:44 +0300

    Add AuthNavigator

---
+++ CarbonApp/src/navigation/AuthNavigator.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Login from '../screens/Login';
+import Register from '../screens/Register';
+import TabNavigator from './TabNavigator';
+
+const Stack = createStackNavigator();
+
+const AuthNavigator = () => {
+    return (
+        <Stack.Navigator screenOptions={{ headerTintColor: "black", headerBackTitleVisible: false }} initialRouteName='Login'>
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="Register" component={Register} />
+            <Stack.Screen name="Main" component={TabNavigator} options={{ headerShown: false }} />
+        </Stack.Navigator>
+    );
+};
+
+export default AuthNavigator;
