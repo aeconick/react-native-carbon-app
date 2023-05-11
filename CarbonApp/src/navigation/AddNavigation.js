6a80aa9eadd5bf955df6f3b2c43bd3f98c836f5a
Nikolay Cherepov <aeconick@gmail.com>
2023-05-11 15:25:21 +0300

    Add nested navigator for add screen

---
+++ CarbonApp/src/navigation/AddNavigation.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Add from '../screens/Add';
+import AddSub from '../screens/AddSub';
+import AddFinal from '../screens/AddFinal';
+import LogoutIcon from '../components/LogoutIcon';
+
+const Stack = createStackNavigator();
+
+const AddNavigator = () => {
+    return (
+        <Stack.Navigator
+            screenOptions={{
+                headerTintColor: "black",
+                headerBackTitleVisible: false,
+                headerRight: () => <LogoutIcon/>
+            }}
+            initialRouteName='Login'
+        >
+            <Stack.Screen name="Add" component={Add} />
+            <Stack.Screen name="AddSub" component={AddSub} options={{
+                headerTitle: 'Add',
+            }} />
+            <Stack.Screen name="Main" component={AddFinal} options={{
+                headerTitle: 'Add',
+            }} />
+        </Stack.Navigator>
+    );
+};
+
+export default AddNavigator;
