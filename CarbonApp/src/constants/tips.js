a387460e6921de42b75e837b3679a6ca6842712e
Nikolay Cherepov <aeconick@gmail.com>
2023-05-26 20:51:18 +0300

    Update tips constant

---
+++ CarbonApp/src/constants/tips.js
@@ -0,0 +1,74 @@
+tips = [
+    {
+        id: 0,
+        title: '1. Drive less',
+        body: 'Driving a car is a major source of greenhouse gasses. Cutting down on the miles you drive is one of the best things you can do for reducing carbon emissions. Organize shopping trips to get more done on each outing, walk or bike when distances are shorter, and use public transportation as much as possible.',
+    },
+    {
+        id: 1,
+        title: '2. Cut down on AC',
+        body: 'Cut usage of the most power-intensive appliance in your car. Running your air conditioning less frequently or raising the temperature so that your system does not work as hard will reduce your carbon footprint.',
+    },
+    {
+        id: 2,
+        title: '3. Avoid flying',
+        body: 'Traveling by air spews more greenhouse gas than traveling by car. To help put this in perspective, a single round-trip flight across the U.S. produces roughly 2 tons of carbon dioxide per person, which equals about 10 percent of a U.S. citizen’s annual carbon footprint. If you can fly less often, it’s an effective way to reduce your carbon footprint.',
+    },
+    {
+        id: 3,
+        title: '4. Eat less meat',
+        body: 'Raising animals requires more energy than growing plants. When you consider using power to produce and ship their food, then ship them to processing and eventually to market, you are talking about a sizable carbon footprint. One of the delicious and healthy ways to reduce your carbon footprint is to switch to high-protein vegetables and certain grains for some of your meals.',
+    },
+    {
+        id: 4,
+        title: '5. Choose organic and local',
+        body: 'It takes energy to plant, cultivate and harvest food. It takes even more to transport it for processing, storage and then distributing it to markets. If you choose , you eliminate that part of the energy use.',
+    },
+    {
+        id: 5,
+        title: '6. Use reusable utensils',
+        body: 'Disposable utensils used once and tossed after a meal are a tremendous waste of resources and contribute to your carbon footprint. Many items are made of plastic derived from oil. The carbon footprint includes the material itself and the energy used in manufacturing and transporting it to you. Once used, it becomes waste that consumes even more energy. Switching to reusable containers has a much lower impact on the environment.',
+    },
+    {
+        id: 6,
+        title: '7. Adjust your thermostat',
+        body: 'When it comes to indoor temperatures, accepting a wider range is one thing you can do to reduce your carbon footprint. If you adjust your thermostat to allow your home to be five degrees colder in the winter and five degrees warmer in the summer, you will cut your energy usage.',
+    },
+    {
+        id: 7,
+        title: '8. Turn it off when not in use',
+        body: 'Many people waste energy by leaving unused lights on for hours. Another common energy-wasting habit is leaving appliances plugged in when not in use. Modern appliances in stand-by mode draw small amounts of power all the time. When you unplug appliances, you can save energy.',
+    },
+    {
+        id: 8,
+        title: '9. Buy only what you need',
+        body: 'We live in a time of abundance, so it is easy to fall into the habit of buying things on a whim that you may end up not really needing. Be mindful when shopping to avoid wasteful purchases that could drive up your carbon footprint.',
+    },
+    {
+        id: 9,
+        title: '10. Bring a reusable bag',
+        body: 'Plastic bags are made from fossil fuels and add to pollution of the planet–particularly in our oceans. Reusable bags are an easy way to eliminate this waste.',
+    },
+    {
+        id: 10,
+        title: '11. Invest in quality products',
+        body: 'Buying cheap, disposable items, from furniture to clothing to kitchen equipment, might seem like a way to economize. You may end up having to replace these items often as they break and wear out. It is better to spend a bit more for lasting quality. That means less energy used in manufacturing and less waste for landfills.',
+    },
+    {
+        id: 11,
+        title: '12. Avoid buying fast fashion',
+        body: 'Fast fashion is inexpensive clothing items that follow the latest trends. Retailers can bring them to the market in weeks, but at a cost. Fast fashion uses more water and energy for manufacturing and shipping, plus the products typically don’t last as long.',
+    },
+    {
+        id: 12,
+        title: '13. Recycle effectively',
+        body: 'Handling waste can be a carbon-intensive process. Recycling reduces the waste going to landfills and gives plastics made from fossil fuels a second life. While recycling programs vary by location, you can recycle common items and help reduce your carbon footprint.',
+    },
+    {
+        id: 13,
+        title: '14. Recycle effectively',
+        body: 'Handling waste can be a carbon-intensive process. Recycling reduces the waste going to landfills and gives plastics made from fossil fuels a second life. While recycling programs vary by location, you can recycle common items and help reduce your carbon footprint.',
+    },
+];
+
+export default tips;
