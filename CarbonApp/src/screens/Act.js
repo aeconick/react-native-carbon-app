8aeb4c19ffb90d66eb7777b6e10892f4f41f7772
Nikolay Cherepov <aeconick@gmail.com>
2023-05-31 17:27:56 +0300

    Fix scroll bug

--- CarbonApp/src/screens/Act.js
+++ CarbonApp/src/screens/Act.js
@@ -7,11 +7,12 @@ import AccordionItem from '../components/AccordionItem';
 const Act = () => {
     return (
         <SafeAreaView>
-            <Text style={{ fontSize: 26, fontWeight: 'bold', margin: 12, color: 'seagreen' }}>
-                Sustainable Guide:
-            </Text>
             <View style={styles.container}>
+                <Text style={{ fontSize: 26, fontWeight: 'bold', margin: 12, color: 'seagreen' }}>
+                    Sustainable Guide:
+                </Text>
                 <FlatList
+                    horizontal={false}
                     data={tips}
                     keyExtractor={(item) => item.id.toString()}
                     renderItem={({ item }) => (
