b63d6cee7a65f281eeb6f137d6773e573c5c3750
Nikolay Cherepov <aeconick@gmail.com>
2023-09-21 10:07:50 +0300

    Update logout to remove session

--- CarbonApp/src/contexts/AuthContext.js
+++ CarbonApp/src/contexts/AuthContext.js
@@ -1,75 +1,89 @@
-import {createContext, useState} from "react";
+import { createContext, useState } from "react";
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import {Alert} from "react-native";
-import {useNavigation} from "@react-navigation/native";
+import { Alert } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 export const AuthContext = createContext();
 
-export const AuthProvider = ({children}) => {
-    const [auth, setAuth] = useState({});
-    const [loading, setLoading] = useState(false);
+export const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState({});
+  const [loading, setLoading] = useState(false);
 
-    const baseUrl = 'http://192.168.1.101:3030';
-    const navigation = useNavigation();
+  const baseUrl = "http://192.168.1.101:3030";
+  const navigation = useNavigation();
 
-    const onLoginSubmit = (inputs) => {
-        setLoading(true);
-        axios.post(`${baseUrl}/users/login`, inputs)
-            .then(function (response) {
-                let userData = response.data;
-                setAuth(userData);
-                AsyncStorage.setItem('userData', JSON.stringify(userData))
-                navigation.navigate("Main");
-                setLoading(false);
-            })
-            .catch(function (error) {
-                setLoading(false);
-                Alert.alert('Error', 'Invalid Details');
-            });
-    };
+  const onLoginSubmit = (inputs) => {
+    setLoading(true);
+    axios
+      .post(`${baseUrl}/users/login`, inputs)
+      .then(function (response) {
+        let userData = response.data;
+        setAuth(userData);
+        AsyncStorage.setItem("userData", JSON.stringify(userData));
+        navigation.navigate("Main");
+        setLoading(false);
+      })
+      .catch(function (error) {
+        setLoading(false);
+        Alert.alert("Error", "Invalid Details");
+      });
+  };
 
-    const onRegisterSubmit = (inputs) => { //TODO: check email
-        setLoading(true);
-        axios.post(`${baseUrl}/users/register`, inputs)
-            .then(function (response) {
-                setLoading(false);
-                navigation.navigate('Login');
-            })
-            .catch(function (error) {
-                setLoading(false);
-                Alert.alert('Error', 'Request Failed');
-            });
-    };
+  const onRegisterSubmit = (inputs) => {
+    //TODO: check email
+    setLoading(true);
+    axios
+      .post(`${baseUrl}/users/register`, inputs)
+      .then(function (response) {
+        setLoading(false);
+        navigation.navigate("Login");
+      })
+      .catch(function (error) {
+        setLoading(false);
+        Alert.alert("Error", "Request Failed");
+      });
+  };
 
-    const onLogoutSubmit = () => { //TODO: logout from server
-        setLoading(true);
+  const onLogoutSubmit = () => {
+    setLoading(true);
+
+    axios
+      .get(`${baseUrl}/users/logout`,{
+        headers: {
+            'x-authorization': auth.accessToken
+        }
+    })
+      .then(function (response) {
         AsyncStorage.clear()
-            .then(() => {
-                setLoading(false);
-                navigation.navigate("Login");
-            })
-            .catch((error) => {
-                setLoading(false);
-                Alert.alert('Error', 'Logout Failed')
-            });
+          .then(() => {
+            setLoading(false);
+            navigation.navigate("Login");
+          })
+          .catch((error) => {
+            setLoading(false);
+            Alert.alert("Error", "Logout Failed");
+          });
 
         setAuth({});
-    };
+      })
+      .catch(function (error) {
+        setLoading(false);
+        Alert.alert("Error", "Request Failed");
+      });
+  };
 
-    const context = {
-        onLoginSubmit,
-        onRegisterSubmit,
-        onLogoutSubmit,
-        loading,
-        auth,
-    };
+  const context = {
+    onLoginSubmit,
+    onRegisterSubmit,
+    onLogoutSubmit,
+    loading,
+    auth,
+  };
 
-    return (
-        <>
-            <AuthContext.Provider value={context}>
-                {children}
-            </AuthContext.Provider>
-        </>
-    );
+  return (
+    <>
+      <AuthContext.Provider value={context}>{children}</AuthContext.Provider>
+    </>
+  );
 };
